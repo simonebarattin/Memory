fc49fb2b3aa10f0e55cc403e0e31d34536546a10
simonebarattin <simonebarattin98@gmail.com>
2016-05-06 22:07:26 +0200

    Card class

---
+++ Card.java
@@ -0,0 +1,83 @@
+package memory;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by simone on 26/04/16.
+ */
+class Card extends JPanel {
+    static int width=200,heigth=280;
+    static int x,y;
+    boolean  state=false, match=false;
+    int id;
+    String front,back="src/memory/back.jpg";
+    Card(String path, int id){
+        this.id=id;
+        front=path;
+    }
+
+    void turn(){
+        if(state) {
+            state = false;
+        }
+        else {
+            state = true;
+        }
+    }
+
+    public void setFront(String front) {
+        this.front = front;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return heigth;
+    }
+
+    public void setX(int x) {
+        Card.x = x;
+    }
+
+    public void setY(int y) {
+        Card.y = y;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setMatch(boolean match) {
+        this.match = match;
+    }
+
+    public boolean getMatch() {
+        return match;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Image img = null;
+        if(state) {
+            try {
+                img = ImageIO.read(new File(front));
+            } catch (IOException e) {
+            }
+            g.drawImage(img, 0, 0, null);
+        }
+        else{
+            try {
+                img = ImageIO.read(new File(back));
+            } catch (IOException e) {
+            }
+            g.drawImage(img, 0, 0, null);
+        }
+    }
+}
