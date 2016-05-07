2a12d72f49e1a3897f76a795d7fe1f3cc7d1e0d0
simonebarattin <simonebarattin98@gmail.com>
2016-05-07 17:31:15 +0200

    Main class

--- Menu.java
+++ Menu.java
@@ -1,61 +1,78 @@
 package memory;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.MediaTracker;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+class Menu extends JPanel implements ActionListener{
+    JLabel title = new JLabel();
+    JButton start = new JButton("Start");
+    JButton credit = new JButton("Credit");
+    JButton exit = new JButton("Exit");
+    static JFrame f = new JFrame();
+    private Image img;
 
-class Menu extends MouseAdapter{
+    public Menu() {
+        setLayout(null);
 
-    JFrame f=new JFrame();
-
-    Menu(){
-        JPanel p=new JPanel();
-        p.setLayout(null);
-
-        JLabel title = new JLabel();
         title.setBounds(113, 34, 198, 60);
-        p.add(title);
+        add(title);
         title.setIcon(new ImageIcon("src/memory/title.png"));
 
-        JLabel start = new JLabel("Start");
         start.setBounds(164, 111, 77, 36);
-        p.add(start);
+        add(start);
 
-        JLabel credit = new JLabel("Credit");
         credit.setBounds(164, 165, 77, 36);
-        p.add(credit);
+        add(credit);
 
-        JLabel exit = new JLabel("Exit");
         exit.setBounds(164, 215, 77, 36);
-        p.add(exit);
+        add(exit);
 
-        start.addMouseListener(this);
-        credit.addMouseListener(this);
-        exit.addMouseListener(this);
+        start.addActionListener(this);
+        credit.addActionListener(this);
+        exit.addActionListener(this);
 
+        img = Toolkit.getDefaultToolkit().createImage("src/memory/frame.jpg");
+        loadImage(img);
+    }
+
+    private void loadImage(Image img) {
+        try {
+            MediaTracker track = new MediaTracker(this);
+            track.addImage(img, 0);
+            track.waitForID(0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void paintComponent(Graphics g) {
+        setOpaque(false);
+        g.drawImage(img, 0, 0, null);
+        super.paintComponent(g);
+    }
+
+    public static void main(String... argv) {
+        Menu back = new Menu();
+        f.getContentPane().add(back);
         f.setBounds(500, 250, 419, 317);
         f.setUndecorated(true);
-        f.getContentPane().add(p);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     @Override
-    public void mouseClicked(MouseEvent e) {
-        // TODO Auto-generated method stub
-        JLabel l=(JLabel)e.getSource();
-        if(l.getText().equals("Start")){
-            new GUI();
+    public void actionPerformed(ActionEvent e) {
+        JButton l = (JButton) e.getSource();
+        if (l.getText().equals("Start")) {
             f.dispose();
-        }
-        else if(l.getText().equals("Exit")){
+            new GUI();
+        } else if (l.getText().equals("Exit")) {
             System.exit(0);
         }
     }
-
 
 }
